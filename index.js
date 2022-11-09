79f3736990eca9209bc1b6bfdc7773671f3623ae
Grouparray <alexjoshiyke@gmail.com>
2022-11-09 06:07:53 +0100

    local update

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
 const express = require('express');
+
 const { json } = require('express');
-const flights = require('./controllers/flightController');
-const models = require('./models/Flight');
-const routes = require('./routes/flightRoute');
+
+const user = require('./router/userRoute');
 
 const app = express();
 
 app.use(json());
 
-app.use('/', routes);
+app.use('/user', user);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000; // port of connect
 
 app.get('/', (req, res) => {
-  res.send('Booking Flight Portal');
+  res.send('Zuri Training Portal');
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Server Started on Port ${PORT}`));
